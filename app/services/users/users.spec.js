e4b57675c08b6dd937fbea0ded58eca1845dee67
Ma. Florencia Masuyama <f.masuyama@globant.com>
2017-01-31 13:44:43 -0300

    Updates on asynchronous callbacks.

--- app/services/users/users.spec.js
+++ app/services/users/users.spec.js
@@ -90,10 +90,76 @@ describe(' $http', function () {
         // 11. check the result.
 
         expect(result).toEqual(returnData);
+    });
+});
 
+////////////////////////////////////////////////////////////////////////////////
+
+//1.
+describe(' asychronous $http', function () {
+    var MyService, httpBackend;
+    var originalTimeout;
+
+    beforeEach(function() {
+      originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
     });
 
+    // it("takes a long time", function(done) {
+    //   setTimeout(function() {
+    //     done();
+    //   }, 9000);
+    // });
+
+    // 2.
+    // 3. load the module.
+    beforeEach(angular.mock.module('App'));
+
+    // 4. get your service, also get $httpBackend
+    // $httpBackend will be a mock.
+    beforeEach(inject(function(_MyService_, $httpBackend) {
+      MyService = _MyService_;
+      httpBackend = $httpBackend;
+    }));
+
+    // 5. make sure no expectations were missed in your tests.
+    afterEach(function () {
+        httpBackend.verifyNoOutstandingExpectation();
+        httpBackend.verifyNoOutstandingRequest();
+    });
+
+    afterEach(function() {
+      jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+    });
+
+    // 6.
+    it(' getArtist()', function (done) {
+
+        setTimeout(function() {
+          done();
+        }, 2000);
 
 
+        var returnData = {};
 
+        // 7. expectGET to make sure this is called once.
+        httpBackend.expectGET("https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg").respond(returnData);
+
+        // 8. make the call.
+        var returnedPromise = MyService.getArtist("0TnOYISbd1XYRBk9myaseg");
+
+        //9. set up a handler for the response, that will put the result
+        // into a variable in this scope for you to test.
+        var result;
+        returnedPromise.then(function (response) {
+            result = response.data;
+        });
+
+        // 10. flush the backend to "execute" the request to do the expectedGET assertion.
+        httpBackend.flush();
+
+        // 11. check the result.
+
+        expect(result).toEqual(returnData);
+    });
 });
